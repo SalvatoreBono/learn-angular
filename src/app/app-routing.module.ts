a2d3aa68f7e6ec4fe3eb0fec7829be420f5d97e5
Salvatore Bono <salvatorebono2001@gmail.com>
2024-02-13 16:56:29 +0100

    add routing incomplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'info',
+    component: InfoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildSendComponent } from './child-send/child-send.component';
+import { ChildComponent } from './child/child.component';
 import { ComponentLifecycleComponent } from './component-lifecycle/component-lifecycle.component';
+import { DirectiveComponent } from './directive/directive.component';
+import { EnterLeaveDirective } from './enter-leave.directive';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { LateralMenuComponent } from './lateral-menu/lateral-menu.component';
 import { MainComponent } from './main/main.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { ParentReceivedComponent } from './parent-received/parent-received.component';
+import { ParentComponent } from './parent/parent.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { ServiceComponent } from './service/service.component';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
-import { NgIfComponent } from './ng-if/ng-if.component';
-import { NgForComponent } from './ng-for/ng-for.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { ParentReceivedComponent } from './parent-received/parent-received.component';
-import { ChildSendComponent } from './child-send/child-send.component';
 import { VariableTemplateComponent } from './variable-template/variable-template.component';
-import { EnterLeaveDirective } from './enter-leave.directive';
-import { DirectiveComponent } from './directive/directive.component';
-import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
@@ -42,6 +44,8 @@ import { ServiceComponent } from './service/service.component';
     EnterLeaveDirective,
     DirectiveComponent,
     ServiceComponent,
+    LateralMenuComponent,
+    InfoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
